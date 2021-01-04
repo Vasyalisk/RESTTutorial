ae59e5855ab960249fb6ac9e36e89b55697724f6
Igor Vasylchenko <vasyalisk@Igor-MacBook-Pro.local>
2021-01-04 11:25:55 +0100

    Tut 4: Added basic authentication for editing snippets. Commit requires new migration in db.

--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,16 @@
 #from django.http import Http404
+from django.contrib.auth.models import User
 
 #from rest_framework import status
 #from rest_framework.decorators import api_view
 #from rest_framework.response import Response
 #from rest_framework.views import APIView
 #from rest_framework import mixins
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 #class SnippetList(APIView):
@@ -51,6 +53,10 @@ class SnippetList(generics.ListCreateAPIView):
     '''
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     
 
 #class SnippetDetail(APIView):
@@ -111,3 +117,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
