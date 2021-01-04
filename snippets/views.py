79a4bbfe797add39393cfbfa44de2b24c640e1ba
Igor Vasylchenko <vasyalisk@Igor-MacBook-Pro.local>
2021-01-04 14:26:29 +0100

    Tut 6: Refactored views into viewsets. Deleted redundant comments.

--- snippets/views.py
+++ snippets/views.py
@@ -1,149 +1,46 @@
-#from django.http import Http404
 from django.contrib.auth.models import User
 
-#from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
-#from rest_framework.views import APIView
-#from rest_framework import mixins
 from rest_framework.reverse import reverse
-from rest_framework import generics, permissions, renderers
+from rest_framework import renderers, viewsets
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
 
-#class SnippetList(APIView):
-#    '''
-#    List all snippets or create a new snippet.
-#    '''
-#    def get(self, request, format=None):
-#        snippets = Snippet.objects.all()
-#        serializer = SnippetSerializer(snippets, many=True)
-#        return Response(serializer.data)
-#
-#    def post(self, request, format=None):
-#        serializer = SnippetSerializer(data=request.data)
-#        if serializer.is_valid():
-#            serializer.save()
-#            return Response(serializer.data, status=status.HTTP_201_CREATED)
-#        else:
-#            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-
-#class SnippetList(mixins.ListModelMixin,
-#                  mixins.CreateModelMixin,
-#                  generics.GenericAPIView):
-#    '''
-#    List all snippets or create a new snippet.
-#    '''
-#    queryset = Snippet.objects.all()
-#    serializer_class = SnippetSerializer
-#
-#    def get(self, request, *a, **kw):
-#        return self.list(request, *a, **kw)
-#
-#    def post(self, request, *a, **kw):
-#        return self.create(request, *a, **kw)
-
-
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
     '''
-    List all snippets or create a new snippet.
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
     '''
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
+    permission_classes = [IsOwnerOrReadOnly, ]
     
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer, ])
+    def highlight(self, request, *a, **kw):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+        
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
     
 
-#class SnippetDetail(APIView):
-#    '''
-#    Retrieve, update or delete a snippet instance.
-#    '''
-#    def _get_object(self, pk): # made helper function private
-#        try:
-#            return Snippet.objects.get(pk=pk)
-#        except Snippet.DoesNotExist:
-#            raise Http404
-#
-#    def get(self, request, pk, format=None):
-#        snippet = self._get_object(pk)
-#        serializer = SnippetSerializer(snippet)
-#        return Response(serializer.data)
-#
-#    def put(self, request, pk, format=None):
-#        snippet = self._get_object(pk)
-#        serializer = SnippetSerializer(snippet, data=request.data)
-#        if serializer.is_valid():
-#            serializer.save()
-#            # Do I need to return whole serializer data?
-#            return Response(serializer.data)
-#        else:
-#            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#    def delete(self, request, pk, format=None):
-#        snippet = self._get_object(pk)
-#        snippet.delete()
-#        return Response(status=status.HTTP_204_NO_CONTENT)
-        
-
-#class SnippetDetail(mixins.RetrieveModelMixin,
-#                    mixins.UpdateModelMixin,
-#                    mixins.DestroyModelMixin,
-#                    generics.GenericAPIView):
-#    '''
-#    Retrieve, update or delete a snippet instance.
-#    '''
-#    queryset = Snippet.objects.all()
-#    serializer_class = SnippetSerializer
-#
-#    # Specify *a an **kw?
-#    def get(self, request, *a, **kw):
-#        return self.retrieve(request, *a, **kw)
-#
-#    def put(self, request, *a, **kw):
-#        return self.update(request, *a, **kw)
-#
-#    def delete(self, request, *a, **kw):
-#        return self.destroy(request, *a, **kw)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     '''
-    Retrieve, update or delete a snippet instance.
+    This viewset automatically provides `list` and `retrieve` actions.
     '''
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    # Unused permission IsAuthenticatedOrReadOnly
-#    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    permission_classes = [IsOwnerOrReadOnly,]
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
-    
-@api_view(['GET',])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-                    })
-                    
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer,]
-    
-    def get(self, request, *a, **kw):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
+
+#@api_view(['GET',])
+#def api_root(request, format=None):
+#    return Response({
+#        'users': reverse('user-list', request=request, format=format),
+#        'snippets': reverse('snippet-list', request=request, format=format),
+#                    })
